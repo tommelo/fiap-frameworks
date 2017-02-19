ceb66e8d30de832cb68388dcb9d77a21fd5ee246
tommelo <mail2.tommelo@gmail.com>
2017-02-19 10:44:46 -0300

    http handler

--- lucene-service/src/main/java/com/fiap/lucene/service/controller/LuceneController.java
+++ lucene-service/src/main/java/com/fiap/lucene/service/controller/LuceneController.java
@@ -1,14 +1,16 @@
 package com.fiap.lucene.service.controller;
 
 import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fiap.lucene.service.domain.ServiceStatus;
-import com.fiap.lucene.service.domain.Ticket;
 import com.fiap.lucene.service.processor.LuceneProcessor;
 
 @RestController("/lucene")
@@ -18,8 +20,14 @@ public class LuceneController {
 	private LuceneProcessor processor;
 	
 	@PostMapping
-	public ServiceStatus process(@RequestBody Ticket ticket) throws IOException {
-		processor.process(ticket);
+	public ServiceStatus process(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		Map<String, String[]> map = request.getParameterMap();
+		for (Map.Entry<String, String[]> entry : map.entrySet()) {
+			System.out.println(entry.getKey());
+			System.out.println(entry.getValue());
+			System.out.println();
+		}
+		//processor.process(ticket);
 		return new ServiceStatus(ServiceStatus.PROCESSING);
 	}
 	
--- ticket-service/src/main/java/com/fiap/ticket/service/controller/TicketController.java
+++ ticket-service/src/main/java/com/fiap/ticket/service/controller/TicketController.java
@@ -3,6 +3,7 @@ package com.fiap.ticket.service.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,6 +19,7 @@ public class TicketController {
 	private TicketRepository repository;
 	
 	@ResponseBody
+	@CrossOrigin("*")
 	@GetMapping("/{id}")
 	public ResponseEntity<TicketResult> get(@PathVariable String id) {
 		TicketResult result = repository.findOne(id);
